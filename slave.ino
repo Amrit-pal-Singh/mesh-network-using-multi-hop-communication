06b5d54d3ab11a0128bef857fd3e4235504e7c49
Amritpal Singh <41193564+Amrit-pal-Singh@users.noreply.github.com>
2019-01-23 19:16:02 +0530

    added slave.ino

---
+++ slave.ino
@@ -0,0 +1,250 @@
+/**
+   ESPNOW - Basic communication - Slave
+   Date: 26th September 2017
+   Author: Arvind Ravulavaru <https://github.com/arvindr21>
+   Purpose: ESPNow Communication between a Master ESP32 and a Slave ESP32
+   Description: This sketch consists of the code for the Slave module.
+   Resources: (A bit outdated)
+   a. https://espressif.com/sites/default/files/documentation/esp-now_user_guide_en.pdf
+   b. http://www.esploradores.com/practica-6-conexion-esp-now/
+   << This Device Slave >>
+   Flow: Master
+   Step 1 : ESPNow Init on Master and set it in STA mode
+   Step 2 : Start scanning for Slave ESP32 (we have added a prefix of `slave` to the SSID of slave for an easy setup)
+   Step 3 : Once found, add Slave as peer
+   Step 4 : Register for send callback
+   Step 5 : Start Transmitting data from Master to Slave
+   Flow: Slave
+   Step 1 : ESPNow Init on Slave
+   Step 2 : Update the SSID of Slave with a prefix of `slave`
+   Step 3 : Set Slave in AP mode
+   Step 4 : Register for receive callback and wait for data
+   Step 5 : Once data arrives, print it in the serial monitor
+   Note: Master and Slave have been defined to easily understand the setup.
+         Based on the ESPNOW API, there is no concept of Master and Slave.
+         Any devices can act as master or salve.
+*/
+
+#include <esp_now.h>
+#include <WiFi.h>
+
+#define CHANNEL 1
+const int DATASIZE = 48;
+uint8_t data1[DATASIZE] = {-1};
+
+// Replace with your network credentials
+const char* ssid     = "Desktop15884";
+const char* password = "19s>2m15";
+
+WiFiServer server(80);
+
+// DHT Sensor
+
+// Temporary variables
+static char celsiusTemp[7];
+static char fahrenheitTemp[7];
+static char humidityTemp[7];
+
+// Client variables 
+char linebuf[80];
+int charcount = 0;
+
+// Init ESP Now with fallback
+void InitESPNow() {
+  WiFi.disconnect();
+  if (esp_now_init() == ESP_OK) {
+    Serial.println("ESPNow Init Success");
+  }
+  else {
+    Serial.println("ESPNow Init Failed");
+    // Retry InitESPNow, add a counte and then restart?
+    // InitESPNow();
+    // or Simply Restart
+    ESP.restart();
+  }
+}
+
+// config AP SSID
+void configDeviceAP() {
+  char* SSID = "ESPNOW_Slave_2";
+  bool result = WiFi.softAP(SSID, "Slave_1_Password", CHANNEL, 4);  //Last parameter is no. of maximum connections
+  if (!result) {
+    Serial.println("AP Config failed.");
+  } else {
+    Serial.println("AP Config Success. Broadcasting with AP: " + String(SSID));
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+  
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.disconnect();
+  WiFi.begin(ssid, password);
+  
+  // attempt to connect to Wifi network:
+  while(WiFi.status() != WL_CONNECTED) {
+    // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  server.begin();
+  
+  Serial.println("ESPNow/Basic/Slave Example");
+  //Set device in AP mode to begin with
+  WiFi.mode(WIFI_AP);
+  // configure device AP mode
+  configDeviceAP();
+  // This is the mac address of the Slave in AP Mode
+  Serial.print("AP MAC: "); Serial.println(WiFi.softAPmacAddress());
+  // Init ESPNow with a fallback logic
+  InitESPNow();
+  // Once ESPNow is successfully Init, we will register for recv CB to
+  // get recv packer info.
+  
+  // We start by connecting to a WiFi network
+  esp_now_register_recv_cb(OnDataRecv);
+  
+}
+
+// callback when data is recv from Master
+void OnDataRecv(const uint8_t *mac_addr, const uint8_t *data, int data_len) {
+  char macStr[18];
+  Serial.print("OnDataRecv called: ");
+  Serial.println(mac_addr[0]);
+  snprintf(macStr, sizeof(macStr), "%02x:%02x:%02x:%02x:%02x:%02x",
+           mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
+  Serial.print("Last Packet Recv from: "); Serial.println(macStr);
+  Serial.print("Last Packet Recv Data: "); 
+  int i = 0;
+  while(i < data_len && data[i] != -1){
+    Serial.print((char)data[i]);
+    i++;
+  }
+    
+  for(int i = 0; i < DATASIZE; i++){
+      data1[i] = data[i];
+  }
+  Serial.println("");
+
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.disconnect();
+  WiFi.begin(ssid, password);
+  
+  // attempt to connect to Wifi network:
+  while(WiFi.status() != WL_CONNECTED) {
+    // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  server.begin();
+  
+}
+
+void loop() {
+  // listen for incoming clients
+  WiFiClient client = server.available();
+  if (client) {
+    Serial.println("New client");
+    memset(linebuf,0,sizeof(linebuf));
+    charcount=0;
+    // an http request ends with a blank line
+    boolean currentLineIsBlank = true;
+    while (client.connected()) {
+      if (client.available()) {
+        char c = client.read();
+        Serial.write(c);
+        //read char by char HTTP request
+        linebuf[charcount]=c;
+        if (charcount<sizeof(linebuf)-1) charcount++;
+        // if you've gotten to the end of the line (received a newline
+        // character) and the line is blank, the http request has ended,
+        // so you can send a reply
+        if (c == '\n' && currentLineIsBlank) {
+          int i = 0;
+          i = 9;
+          humidityTemp[0] = data1[i++];
+          humidityTemp[1] = data1[i++];
+          humidityTemp[2] = data1[i++];
+          i++;i++;
+          celsiusTemp[0] = data1[i++];
+          celsiusTemp[1] = data1[i++];
+          i++;i++;
+          fahrenheitTemp[0] = data1[i++];
+          fahrenheitTemp[1] = data1[i++];
+          fahrenheitTemp[2] = data1[i++];
+
+
+          Serial.print("Humidity: ");
+          Serial.print(humidityTemp[0]);
+          Serial.print(humidityTemp[1]); 
+          Serial.print(humidityTemp[2]);
+          Serial.print(" %\t Temperature: ");
+          Serial.print(celsiusTemp[0]);
+          Serial.print(celsiusTemp[1]);
+          Serial.print(" *C ");
+          Serial.print(fahrenheitTemp[0]);
+          Serial.print(fahrenheitTemp[1]); 
+          Serial.print(fahrenheitTemp[2]);
+          Serial.print(" *F");
+
+                    
+          // send a standard http response header
+          client.println("HTTP/1.1 200 OK");
+          client.println("Content-Type: text/html");
+          client.println("Connection: close");  // the connection will be closed after completion of the response
+          client.println();
+          client.println("<!DOCTYPE HTML><html><head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+          client.println("<meta http-equiv=\"refresh\" content=\"30\"></head>");
+          client.println("<body><div style=\"font-size: 3.5rem;\"><p>ESP32 - DHT</p><p>");
+          if(atoi(celsiusTemp)>=25){
+            client.println("<div style=\"color: #930000;\">");
+          }
+          else if(atoi(celsiusTemp)<25 && atoi(celsiusTemp)>=5){
+            client.println("<div style=\"color: #006601;\">");
+          }
+          else if(atoi(celsiusTemp)<5){
+            client.println("<div style=\"color: #009191;\">");
+          }
+          client.println(celsiusTemp);
+          client.println("*C</p><p>");
+          client.println(fahrenheitTemp);
+          client.println("*F</p></div><p>");
+          client.println(humidityTemp);
+          client.println("%</p></div>");
+          client.println("</body></html>");     
+          break;
+        }
+        if (c == '\n') {
+          // you're starting a new line
+          currentLineIsBlank = true;
+          memset(linebuf,0,sizeof(linebuf));
+          charcount=0;
+        } else if (c != '\r') {
+          // you've gotten a character on the current line
+          currentLineIsBlank = false;
+        }
+      }
+    }
+    // give the web browser time to receive the data
+    delay(1);
+
+    // close the connection:
+    client.stop();
+    Serial.println("client disconnected");
+  }
+}
